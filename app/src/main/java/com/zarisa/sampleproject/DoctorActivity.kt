b33be8a82a5fa0793825b7505a4724ba0aa4d41f
Zarisa <jannesari.zarisa@gmail.com>
2022-03-13 00:13:15 +0330

    set data from view model in doctor page

--- app/src/main/java/com/zarisa/sampleproject/DoctorActivity.kt
+++ app/src/main/java/com/zarisa/sampleproject/DoctorActivity.kt
@@ -1,5 +1,6 @@
 package com.zarisa.sampleproject
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
@@ -8,11 +9,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import com.zarisa.sampleproject.databinding.ActivityDoctorBinding
 
 
 class DoctorActivity : AppCompatActivity() {
     lateinit var binding: ActivityDoctorBinding
+    val viewModel2: ViewModel2 by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDoctorBinding.inflate(layoutInflater)
@@ -21,25 +24,34 @@ class DoctorActivity : AppCompatActivity() {
         primaryInit()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun primaryInit() {
-        val firstDoctor = Hospital.doctorList[1]
-        binding.textViewName.text = firstDoctor.name
-        binding.textViewField.text = firstDoctor.field
-        binding.textViewConnection.text = firstDoctor.connection.toString()
+//        val firstDoctor = Hospital.doctorList[1]
+//        binding.textViewName.text = firstDoctor.name
+//        binding.textViewField.text = firstDoctor.field
+//        binding.textViewConnection.text = firstDoctor.connection.toString()
+//
+//        binding.llConsultancy10.setOnClickListener {
+//            Toast.makeText(this , "cunsultancy is chosen" , Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this , ConsultActivity::class.java)
+//            intent.putExtra("id" , firstDoctor.id)
+//            startActivity(intent)
+//
+//            // startForResult.launch(intent)
+//        }
+//
+//    }
 
-        var cons1 = Hospital.consultancyList[0]
-        binding.textViewConsultancy10.text = " مشاوره تلفنی " + cons1.time + " دقیقه ای "
-        binding.textViewConsultancy10.text = " مشاوره تلفنی " + cons1.time + " دقیقه ای "
-        binding.textViewConsultancyPrice10.text = cons1.price.toString() +  " تومان "
-
-        binding.llConsultancy10.setOnClickListener {
-            Toast.makeText(this , "cunsultancy is chosen" , Toast.LENGTH_SHORT).show()
-            val intent = Intent(this , ConsultActivity::class.java)
-            intent.putExtra("id" , firstDoctor.id)
-            startActivity(intent)
-
-            // startForResult.launch(intent)
-        }
+        var doctorNumber=intent.getIntExtra("chosenButton",1)
+        binding.textViewName.text=viewModel2.getDoctorName(doctorNumber-1)
+        binding.textViewField.text=viewModel2.getDoctorExpertise(doctorNumber-1)
+        binding.textViewConnection.text=viewModel2.getConectionState(doctorNumber-1).toString()
+        binding.textViewConsultancy10.text=" مشاوره تلفنی " + viewModel2.getConsultancyTime(0) + " دقیقه ای "
+        binding.textViewConsultancy30.text=" مشاوره تلفنی " + viewModel2.getConsultancyTime(1) + " دقیقه ای "
+        binding.textViewConsultancy60.text=" مشاوره تلفنی " + viewModel2.getConsultancyTime(2) + " دقیقه ای "
+        binding.textViewConsultancyPrice10.text= viewModel2.getConsultancyPrise(0).toString() +  " تومان "
+        binding.textViewConsultancyPrice30.text= viewModel2.getConsultancyPrise(1).toString() +  " تومان "
+        binding.textViewConsultancyPrice60.text= viewModel2.getConsultancyPrise(2).toString() +  " تومان "
         binding.buttonSendMassege.setOnClickListener {
             val callIntent = Intent(Intent.ACTION_DIAL)
             callIntent.data = Uri.parse("tel:09121234567")
--- app/src/main/java/com/zarisa/sampleproject/ViewModels.kt
+++ app/src/main/java/com/zarisa/sampleproject/ViewModels.kt
@@ -9,5 +9,19 @@ class ViewModel1 : ViewModel(){
     }
 }
 class ViewModel2:ViewModel(){
-
+    fun getConsultancyTime(numberOfConsultancy:Int):Int{
+        return Hospital.consultancyList[numberOfConsultancy].time
+    }
+    fun getConsultancyPrise(numberOfConsultancy:Int):Int{
+        return Hospital.consultancyList[numberOfConsultancy].price
+    }
+    fun getDoctorName(numberOfDoctor: Int):String{
+        return Hospital.doctorList[numberOfDoctor].name
+    }
+    fun getDoctorExpertise(numberOfDoctor: Int):String{
+        return Hospital.doctorList[numberOfDoctor].field
+    }
+    fun getConectionState(numberOfDoctor: Int):Connection{
+        return Hospital.doctorList[numberOfDoctor].connection
+    }
 }
